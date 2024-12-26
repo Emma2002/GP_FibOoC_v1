38647b3c0919199bee6e2d1ad7e0f6f2ab11ee82
EmmavandenBroek <emmamyrthe@gmail.com>
2024-12-26 14:12:13 +0100

    Added score counter for correct answered questions

--- Assets/Scripts/ConversationStarters/ConversationStarter.cs
+++ Assets/Scripts/ConversationStarters/ConversationStarter.cs
@@ -1,18 +1,42 @@
 using UnityEngine;
 using DialogueEditor;
+using TMPro;
 
 public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int AnsweredCorrectCount;
+    private bool HasAnsweredCorrect = false;
+    [SerializeField] private TextMeshProUGUI scoreText;
     void Start()
     {
         ConversationManager.Instance.StartConversation(myConversation);
+        UpdateScoreText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(ConversationManager.Instance.GetBool("AnsweredCorrect") == true && !HasAnsweredCorrect)
+        {
+            AnsweredCorrectCount += 1;
+            HasAnsweredCorrect = true;
+            UpdateScoreText();
+        }
+        if(ConversationManager.Instance.GetBool("AnsweredCorrect") == false)
+        {
+            HasAnsweredCorrect = false;
+            UpdateScoreText();
+        }
+        Debug.Log("Answered Correct count2: " + AnsweredCorrectCount);
+    }
+
+     private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = $"Score: {AnsweredCorrectCount}";
+        }
     }
 }
