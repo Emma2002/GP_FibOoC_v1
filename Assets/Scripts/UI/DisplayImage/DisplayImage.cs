4d4cbe5ac531100b7b51c7e6941776f1b2337d19
EmmavandenBroek <emmamyrthe@gmail.com>
2024-12-29 22:16:22 +0100

    Position dialogue screen changes during video

--- Assets/Scripts/UI/DisplayImage/DisplayImage.cs
+++ Assets/Scripts/UI/DisplayImage/DisplayImage.cs
@@ -5,28 +5,43 @@ public class DisplayImage : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private GameObject imageComponent;
+    [SerializeField] private string startVideoBool;
+    [SerializeField] private GameObject dialogueScreen;
+
+    [SerializeField] private float yPositionChange;
+
+    private float originalFloatY;
     void Start()
     {
         // Get the Image component and hide the image initially
         imageComponent.gameObject.SetActive(false);
+
+        originalFloatY = dialogueScreen.transform.position.y;
+        Debug.Log("The original Float issssssss: " + originalFloatY);
+        
+        // SetDialogueScreenPosition(false);
       
     }
 
     void Update()
     {
         // Check if the boolean has changed and update the image visibility
-        bool showImage = ConversationManager.Instance.GetBool("ShowImageOrVideo");
+        bool showImage = ConversationManager.Instance.GetBool(startVideoBool);
         Debug.Log("Status of the image bool: " + showImage);
-        Debug.Log(ConversationManager.Instance.GetBool("ShowImageOrVideo"));
+        Debug.Log(ConversationManager.Instance.GetBool(startVideoBool));
         
-        if (ConversationManager.Instance.GetBool("ShowImageOrVideo") == true)
+        if (ConversationManager.Instance.GetBool(startVideoBool) == true)
         {
             imageComponent.gameObject.SetActive(true);
+            dialogueScreen.transform.position = new Vector3(dialogueScreen.transform.position.x, originalFloatY + yPositionChange, dialogueScreen.transform.position.z);
+
         }
-        if (ConversationManager.Instance.GetBool("ShowImageOrVideo") == false)
+        if (ConversationManager.Instance.GetBool(startVideoBool) == false)
         {
             imageComponent.gameObject.SetActive(false);
+            dialogueScreen.transform.position = new Vector3(dialogueScreen.transform.position.x, originalFloatY, dialogueScreen.transform.position.z);
+
         }   
     }
-}
 
+}
