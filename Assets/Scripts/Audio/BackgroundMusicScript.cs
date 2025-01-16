16005cecc5be3f49499e7c7487716c41ef47ddf9
Emma <emmamyrthe@gmail.com>
2025-01-16 13:57:43 +0100

    Background music added

---
+++ Assets/Scripts/Audio/BackgroundMusicScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BackgroundMusicScript : MonoBehaviour
+{
+    public static BackgroundMusicScript instance;
+    
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+}
