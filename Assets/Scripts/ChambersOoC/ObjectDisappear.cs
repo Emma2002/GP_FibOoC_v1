6b892cd08205f600cf6466a660d00e48cebe86e6
Emma <emmamyrthe@gmail.com>
2025-01-17 14:52:52 +0100

    Added ECM structures that disappear

--- Assets/Scripts/ChambersOoC/ObjectDisappear.cs
+++ Assets/Scripts/ChambersOoC/ObjectDisappear.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
 using DialogueEditor;
+using System.Collections.Generic; // Include this namespace for List<T>
 
 public class ObjectDisappear : MonoBehaviour
 {
-   [SerializeField]  List<GameObject> disappearingObjects;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private List<GameObject> disappearingObjects; // Best practice: explicitly declare access modifiers
 
     // Update is called once per frame
     void Update()
     {
-        foreach(GameObject object in disappearingObjects){
+        if (ConversationManager.Instance.GetBool("ECMDialogueFinished") == true)
+        {
+            foreach (GameObject obj in disappearingObjects) // Changed 'object' to 'obj'
+            {
+                obj.SetActive(false);
+            }
         }
     }
 }
